fe6a8856ef2c29f6948ebdb6fb43a6a560e2b031
Harm Aarts <harmaarts@gmail.com>
2018-11-19 15:58:51 +0100

    Implement P2WPKH/P2PKH address generation

--- lib/src/discover.dart
+++ lib/src/discover.dart
@@ -1,3 +1,24 @@
+import 'package:bech32/bech32.dart';
+import 'package:pointycastle/ecc/api.dart';
+import 'package:pointycastle/digests/sha256.dart';
+import 'package:pointycastle/digests/ripemd160.dart';
+import 'package:base58check/base58check.dart';
+
+final sha256 = SHA256Digest();
+final ripemd160 = RIPEMD160Digest();
+
 abstract class Scanner {
   Future<bool> present(String address);
+}
+
+String toP2WPKH(ECPoint publicKey) {
+  var bytes = publicKey.getEncoded(true);
+  var hashed = ripemd160.process(sha256.process(bytes));
+  return segwit.encode(Segwit('bc', 0, hashed));
+}
+
+String toP2PKH(ECPoint publicKey) {
+  var bytes = publicKey.getEncoded(true);
+  var hashed = ripemd160.process(sha256.process(bytes));
+  return Base58CheckCodec.bitcoin().encode(Base58CheckPayload(0, hashed));
 }
--- test/discovery_test.dart
+++ test/discovery_test.dart
@@ -1,16 +1,37 @@
 import 'dart:io';
 
-import 'package:test/test.dart';
+import 'package:hex/hex.dart';
 import 'package:mock_web_server/mock_web_server.dart';
+import "package:pointycastle/ecc/api.dart";
+import "package:pointycastle/ecc/curves/secp256k1.dart";
+import 'package:test/test.dart';
 
-import 'package:bitcoin_bip44/bitcoin_bip44.dart';
-import '../lib/src/discover/blockchair.dart';
 import '../lib/src/discover.dart';
+import '../lib/src/discover/blockchair.dart';
 
 void main() {
   group("address genenration with given public key", () {
-    test("generate P2PKH", () {});
-    test("generate P2WPKH", () {});
+    var publicKey = HEX.decode(
+        "0279BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798");
+    ECPoint point;
+
+    setUp(() {
+      var curve = ECCurve_secp256k1().curve;
+      point = curve.decodePoint(publicKey);
+    });
+
+    test("generate P2PKH", () {
+      var expectedAddress = "1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH";
+
+      expect(toP2PKH(point), expectedAddress);
+    });
+
+    test("generate P2WPKH", () {
+      // Public key -> address is taken from BIP173 examples: https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#Examples
+      var expectedSegwitAddress = "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4";
+
+      expect(toP2WPKH(point), expectedSegwitAddress);
+    });
   });
 
   group("address scanning", () {
