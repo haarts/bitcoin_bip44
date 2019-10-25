0d25681a7014c50073d60dea4bda372c8e1225fc
Dominik Spicher <dominik.spicher@inacta.ch>
2019-10-25 16:27:19 +0200

    Fix errors in example

--- example/main.dart
+++ example/main.dart
@@ -3,14 +3,14 @@ import "package:bitcoin_bip44/bitcoin_bip44.dart";
 void main() {
   Bip44 bip44 = Bip44(toHexString("some seed"));
   Coin bitcoin = bip44.coins[0];
-  account = Account(bitcoin, 0, changeExternal);
-  Address unUsed = account.nextUnusedAddress();
+  var account = Account(bitcoin, 0, changeExternal);
+  account.nextUnusedAddress().then((address) => print(address));
 
   // Add a scanner of your own:
   scanners = [MyOwnScanner()];
 }
 
-class MyOwnScanner {
+class MyOwnScanner extends Scanner {
   Future<bool> present(String address) {
     // Total nonsense!
     if (address.endsWith('b')) {
